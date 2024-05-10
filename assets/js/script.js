6c321c0d26cfac1cd0b195418a96899933fc19ea
C-ITO <C-ITO@kan-adminnoMacBook-Air.local>
2024-05-10 14:25:17 +0900

    First Commit

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+/*********************
+ * index
+ * -------------------
+ * 0. common
+ * 1. header / footer
+ * 2. Swiper
+ *********************/
+
+/*********************
+ * 0. common
+ *********************/
+
+/*********************
+ * 1. header / footer
+ *********************/
+
+/*********************
+ * 2. Swiper
+ *********************/
+const mvSwiper = new Swiper('.top__mv .swiper', {
+  loop: true,
+  slidesPerView: 1,
+  speed: 1000,
+  autoplay: {
+    delay: 10000,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
+});
