ed608b40ebf4088d554af2959cab79cfb015b7ab
C-ITO <C-ITO@kan-adminnoMacBook-Air.local>
2024-07-09 18:31:17 +0900

    修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,6 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-    // 出力ディレクトリを指定
-    assetsDir: 'assets' // アセットディレクトリを指定
-  },
   output: "server",
   adapter: vercel()
 });
